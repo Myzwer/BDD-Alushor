65d04f6e025b81089d5ce3bb462bee5995d564a4
Myzwer <josh@onefortyfivedesign.com>
2023-02-14 01:48:57 -0500

    :repeat: Refactored the svgs so they are in their own components and not clogging up my files. The color class gets passed in as a prop.

---
+++ components/BottomWave.vue
@@ -0,0 +1,21 @@
+<template>
+	<svg
+		:class="['fill-current', color]" viewBox="0 0 1737 181" fill="none"
+		xmlns="http://www.w3.org/2000/svg">
+		<path
+			fill-rule="evenodd" clip-rule="evenodd"
+			d="M0.5 74.5L72.5 83.5C145.5 92.5 289.5 109.5 434.5 127.5C579.5 144.5 723.5 161.5 868.5 135.5C1013.5 109.5 1157.5 40.5 1302.5 14.5C1447.5 -11.5 1591.5 5.49998 1664.5 14.5L1736.5 22.5V181H1664.5C1591.5 181 1447.5 181 1302.5 181C1157.5 181 1013.5 181 868.5 181C723.5 181 579.5 181 434.5 181C289.5 181 145.5 181 72.5 181H0.5L0.5 74.5Z"/>
+	</svg>
+</template>
+
+<script>
+export default {
+	name: "BottomWave",
+	props: {
+		color: {
+			type: String,
+			default: ''
+		}
+	}
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,7 @@
 		<div class="relative">
 			<div class="index-h1-background relative">
 				<div class="absolute z-2 w-full">
-					<svg
-						class="fill-current text-primary" viewBox="0 0 1736 172" fill="none"
-						xmlns="http://www.w3.org/2000/svg">
-						<path
-							fill-rule="evenodd" clip-rule="evenodd"
-							d="M1736 98L1664 89C1591 80 1447 63 1302 45.0001C1157 28.0001 1013 11.0001 868 37.0001C723 63.0001 579 132 434 158C289 184 145 167 72 158L-1.90808e-06 150L-1.50215e-05 7.5472e-05L72 6.91776e-05C145 6.27957e-05 289 5.02068e-05 434 3.75305e-05C579 2.48542e-05 723 1.22653e-05 868 -4.10975e-07C1013 -1.30873e-05 1157 -2.56762e-05 1302 -3.83525e-05C1447 -5.10288e-05 1591 -6.36176e-05 1664 -6.99995e-05L1736 -7.62939e-05L1736 98Z"/>
-					</svg>
+					<TopWave color="text-primary" />
 				</div>
 				<div class="content-middle text-white text-center">
 					<h1 class="text-8xl mb-2 px-2">
@@ -21,13 +15,7 @@
 				</div>
 			</div>
 			<div class="absolute bottom-0 z-2 w-full">
-				<svg
-					class="fill-current text-secondary" viewBox="0 0 1737 181" fill="none"
-					xmlns="http://www.w3.org/2000/svg">
-					<path
-						fill-rule="evenodd" clip-rule="evenodd"
-						d="M0.5 74.5L72.5 83.5C145.5 92.5 289.5 109.5 434.5 127.5C579.5 144.5 723.5 161.5 868.5 135.5C1013.5 109.5 1157.5 40.5 1302.5 14.5C1447.5 -11.5 1591.5 5.49998 1664.5 14.5L1736.5 22.5V181H1664.5C1591.5 181 1447.5 181 1302.5 181C1157.5 181 1013.5 181 868.5 181C723.5 181 579.5 181 434.5 181C289.5 181 145.5 181 72.5 181H0.5L0.5 74.5Z"/>
-				</svg>
+				<BottomWave color="text-secondary" />
 			</div>
 		</div>
 		<div class="bg-secondary py-10">
@@ -48,13 +36,7 @@
 		</div>
 
 		<div class="bg-primary">
-			<svg
-				class="fill-current text-secondary" viewBox="0 0 1736 172" fill="none"
-				xmlns="http://www.w3.org/2000/svg">
-				<path
-					fill-rule="evenodd" clip-rule="evenodd"
-					d="M1736 98L1664 89C1591 80 1447 63 1302 45.0001C1157 28.0001 1013 11.0001 868 37.0001C723 63.0001 579 132 434 158C289 184 145 167 72 158L-1.90808e-06 150L-1.50215e-05 7.5472e-05L72 6.91776e-05C145 6.27957e-05 289 5.02068e-05 434 3.75305e-05C579 2.48542e-05 723 1.22653e-05 868 -4.10975e-07C1013 -1.30873e-05 1157 -2.56762e-05 1302 -3.83525e-05C1447 -5.10288e-05 1591 -6.36176e-05 1664 -6.99995e-05L1736 -7.62939e-05L1736 98Z"/>
-			</svg>
+			<TopWave color = "text-secondary" />
 			<div class="text-center mx-auto max-w-2xl md:max-w-5xl">
 
 				<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
@@ -137,15 +119,7 @@
 		</div>
 
 		<div class="bg-secondary">
-
-			<svg
-				class="fill-current text-primary" viewBox="0 0 1736 172" fill="none"
-				xmlns="http://www.w3.org/2000/svg">
-				<path
-					fill-rule="evenodd" clip-rule="evenodd"
-					d="M1736 98L1664 89C1591 80 1447 63 1302 45.0001C1157 28.0001 1013 11.0001 868 37.0001C723 63.0001 579 132 434 158C289 184 145 167 72 158L-1.90808e-06 150L-1.50215e-05 7.5472e-05L72 6.91776e-05C145 6.27957e-05 289 5.02068e-05 434 3.75305e-05C579 2.48542e-05 723 1.22653e-05 868 -4.10975e-07C1013 -1.30873e-05 1157 -2.56762e-05 1302 -3.83525e-05C1447 -5.10288e-05 1591 -6.36176e-05 1664 -6.99995e-05L1736 -7.62939e-05L1736 98Z"/>
-			</svg>
-
+			<TopWave color = "text-primary" />
 
 			<div class="text-center mx-auto max-w-2xl md:max-w-5xl">
 				<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
@@ -177,24 +151,19 @@
 
 				</div>
 			</div>
-			<svg
-				class="fill-current text-primary" viewBox="0 0 1737 181" fill="none"
-				xmlns="http://www.w3.org/2000/svg">
-				<path
-					fill-rule="evenodd" clip-rule="evenodd"
-					d="M0.5 74.5L72.5 83.5C145.5 92.5 289.5 109.5 434.5 127.5C579.5 144.5 723.5 161.5 868.5 135.5C1013.5 109.5 1157.5 40.5 1302.5 14.5C1447.5 -11.5 1591.5 5.49998 1664.5 14.5L1736.5 22.5V181H1664.5C1591.5 181 1447.5 181 1302.5 181C1157.5 181 1013.5 181 868.5 181C723.5 181 579.5 181 434.5 181C289.5 181 145.5 181 72.5 181H0.5L0.5 74.5Z"/>
-			</svg>
+			<BottomWave color = "text-primary" />
 		</div>
 	</div>
 </template>
 
 <script>
 import CountdownToDate from '~/components/CountdownToDate'
+import TopWave from "~/components/TopWave";
 import {index, RSVP, groom, bride, tagline, couple} from '~/data/data.json'
 
 export default {
 	name: 'IndexPage',
-	components: {CountdownToDate},
+	components: {CountdownToDate, TopWave},
 
 	data() {
 		return {
