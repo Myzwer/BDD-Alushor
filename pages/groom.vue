15087fc606f97dcb2869b48a12d4821c71c35d86
Myzwer <josh@onefortyfivedesign.com>
2023-02-15 00:00:48 -0500

    :art: Knocked out the groom page design to match the mockup.

--- pages/groom.vue
+++ pages/groom.vue
@@ -2,6 +2,9 @@
     <div>
         <div class="relative">
             <div class="groom-h1-background">
+				<div class="absolute z-2 w-full">
+					<TopWave color="text-primary"/>
+				</div>
                 <div class="content-middle text-white text-center">
                     <h1 class="text-7xl mb-2 px-2">Meet The Groom</h1>
                     <h2 class="text-xl font-bold uppercase body-font">
@@ -9,23 +12,25 @@
                     </h2>
                 </div>
             </div>
+			<div class="absolute bottom-0 z-2 w-full">
+				<BottomWave color="text-secondary"/>
+			</div>
         </div>
 
-        <div class="bg-secondary">
-            <div class="text-center mx-auto max-w-2xl md:max-w-5xl">
-                <div class="grid grid-cols-12 gap-4 pt-10 pb-12">
-                    <div class="col-span-12 md:col-span-5 px-5">
-                        <div class="mt-10 px-5">
+		<div class="bg-secondary">
+			<div class="text-center mx-auto max-w-2xl md:max-w-3xl">
+				<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
+					<div class="col-span-12 md:col-start-3 md:col-span-8 px-5">
+						<div class="mt-10 px-5">
                             <img
-                                class="rounded-xl shadow-lg"
                                 :src="groomHeadshot"
                                 alt="Groom Headshot"
                             />
                         </div>
                     </div>
 
-                    <div class="col-span-12 md:col-span-7 px-5">
-                        <h2 class="pt-7 text-5xl text-left">
+					<div class="col-span-12 px-5 pb-5">
+						<h2 class="pt-7 pb-5 text-7xl text-center">
                             Hey! I'm {{ firstName }}.
                         </h2>
                         <p class="text-left pb-5">
@@ -35,45 +40,35 @@
                         <p class="text-left pb-5">
                             {{ bio2 }}
                         </p>
-                        <div v-if="facebook != null" class="pb-5">
-                            <a :href="facebook" target="_blank">
-                                <div
-                                    class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent hover:text-white hover:border-white"
-                                >
+						<div class="text-left">
+							<span v-if="facebook != null" class="pb-5 text-4xl text-black">
+								<a :href="facebook" target="_blank">
+									<font-awesome-icon
+										icon="fa-brands fa-facebook"
+									/>
+								</a>
+							</span>
+
+							<span v-if="instagram != null" class="pb-5 text-4xl text-black">
+                            	<a :href="instagram" target="_blank">
                                     <font-awesome-icon
-                                        icon="fa-brands fa-facebook"
-                                    />
-                                    Facebook
-                                </div>
-                            </a>
-                        </div>
-                        <div v-if="instagram != null" class="pb-5">
-                            <a :href="instagram" target="_blank">
-                                <div
-                                    class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent hover:text-white hover:border-white"
-                                >
+										icon="fa-brands fa-instagram"
+									/>
+                            	</a>
+                        	</span>
+
+							<span v-if="twitter != null" class="pb-5 text-4xl text-black">
+                            	<a :href="twitter" target="_blank">
                                     <font-awesome-icon
-                                        icon="fa-brands fa-instagram"
-                                    />
-                                    Instagram
-                                </div>
-                            </a>
-                        </div>
-                        <div v-if="twitter != null" class="pb-5">
-                            <a :href="twitter" target="_blank">
-                                <div
-                                    class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent hover:text-white hover:border-white"
-                                >
-                                    <font-awesome-icon
-                                        icon="fa-brands fa-twitter"
-                                    />
-                                    Twitter
-                                </div>
-                            </a>
-                        </div>
+										icon="fa-brands fa-twitter"
+									/>
+                            	</a>
+                        	</span>
+						</div>
                     </div>
                 </div>
             </div>
+			<BottomWave color = "text-primary-alt" />
         </div>
 
         <button-footer />
